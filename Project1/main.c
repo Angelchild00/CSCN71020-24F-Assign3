bef9de579639ad6cddd9dbdd7ae72833b09555b6
Angelchild00 <leanne.elizabeth@gmail.com>
2024-10-29 12:55:40 -0400

    adding comments to source code

--- Project1/main.c
+++ Project1/main.c
@@ -14,10 +14,10 @@ int main(void)
 
 char* Game(char* player1, char* player2) {
 
-	if (strcmp(player1, player2) == 0) {
+	if (strcmp(player1, player2) == 0) { //returns draw if both players pick the same
 		return "Draw";
 	}
-	 else if (strcmp(player1, "rock") == 0 && strcmp(player2, "paper") == 0) {
+	 else if (strcmp(player1, "rock") == 0 && strcmp(player2, "paper") == 0) { //player one winning
 		return "Player1";
 	}
 	else if (strcmp(player1, "scissors") == 0 && strcmp(player2, "paper") == 0) {
@@ -26,7 +26,7 @@ char* Game(char* player1, char* player2) {
 	else if (strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0) {
 		return "Player1";
 	}
-	else if (strcmp(player2, "rock") == 0 && strcmp(player1, "paper") == 0) {
+	else if (strcmp(player2, "rock") == 0 && strcmp(player1, "paper") == 0) { //player two winning
 		return "Player2";
 	}
 	else if (strcmp(player2, "scissors") == 0 && strcmp(player1, "paper") == 0) {
@@ -36,6 +36,6 @@ char* Game(char* player1, char* player2) {
 		return "Player2";
 	}
 	
-	return "Invalid";
+	return "Invalid"; //if any input other than rock, paper, or scissors is used
 	
 }
